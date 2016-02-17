a1965f2086b975c129197deb1fdeb1bdc0ccf1b2
moneyache <chanteng@qq.com>
2016-02-17 17:33:43 +0800

    update

--- main/algorithm/travel/calculate_expression1.py
+++ main/algorithm/travel/calculate_expression1.py
@@ -4,67 +4,81 @@ __author__ = 'teng.qian'
 class Solution(object):
 
     @staticmethod
-    def recurse(index, s, max_index, stack, is_start):
-        if index >= max_index:
-            return index, Solution.cal_stack(stack)
-        c = s[index]
-        if c == ' ':
-            return Solution.recurse(index + 1, s, max_index, stack, is_start)
-        if c == '(':
-            if len(stack) == 0:
-                return Solution.recurse(index + 1, s, max_index, stack, True)
-            if (stack[len(stack) - 1]) == '+':
-                return Solution.recurse(index + 1, s, max_index, stack, True)
-            index, v = Solution.recurse(index + 1, s, max_index, [], is_start)
-            stack.append(v)
-            return Solution.recurse(index + 1, s, max_index, stack, is_start)
-        elif c == ')':
-            if is_start:
-                return Solution.recurse(index + 1, s, max_index, stack, False)
-            return index, Solution.cal_stack(stack)
-        elif (c == '+') | (c == '-'):
-            last = Solution.cal_stack(stack)
-            stack.append(last)
-            stack.append(c)
-        else:
-            temp = len(stack)
-            if temp == 0:
-                stack.append(int(c))
-            else:
-                later = stack[temp - 1]
-                if (later == '+') | (later == '-'):
-                    stack.append(int(c))
+    def to_postfix(s):
+        s = "(" + s + ")"
+        result = []
+        last = 0
+        right = False
+        temp = []
+        for cur in s:
+            if Solution.is_left_bracket(cur):
+                temp.append(cur)
+            elif Solution.is_right_bracket(cur):
+                if not right:
+                    result.append(last)
+                    last = 0
+                h = temp.pop()
+                while h != '(':
+                    result.append(h)
+                    h = temp.pop()
+                right = True
+            elif Solution.is_operate(cur):
+                if right:
+                    right = False
                 else:
-                    stack[temp - 1] = (later*10 + int(c))
-        return Solution.recurse(index + 1, s, max_index, stack, is_start)
-
-
-
-    @staticmethod
-    def cal_stack(stack):
-        if len(stack) >= 3:
-            right = stack.pop()
-            return Solution.count(stack.pop(), stack.pop(), right)
-        return stack.pop()
+                    result.append(last)
+                    last = 0
+                while len(temp) > 0:
+                    last_symbol = temp.pop()
+                    if last_symbol == '(':
+                        temp.append(last_symbol)
+                        break
+                    result.append(last_symbol)
+                temp.append(cur)
+            elif Solution.is_number(cur):
+                last = last * 10 + int(cur)
+        return result
 
     def calculate(self, s):
         if len(s) <= 1:
             return int(s)
-        return Solution.recurse(0, s, len(s), [], False)[1]
+        prefix = Solution.to_postfix(s)
+        stack = []
+        for e in prefix:
+            if Solution.is_operate(e):
+                right = stack.pop()
+                stack.append(Solution.count(e, stack.pop(), right))
+            else:
+                stack.append(e)
+        return stack.pop()
 
 
     @staticmethod
     def count(label, left, right):
         if label == "+":
-            return int(left) + int(right)
+            return left + right
         if label == "-":
-            return int(left) - int(right)
+            return left - right
         if label == "*":
-            return int(left) * int(right)
+            return left * right
         if label == "/":
-            return int(left) / int(right)
+            return left / right
+
+    @staticmethod
+    def is_left_bracket(c):
+        return c == '('
+    @staticmethod
+    def is_right_bracket(c):
+        return c == ')'
+    @staticmethod
+    def is_number(c):
+        return (c >= '0') & (c <= '9')
+    @staticmethod
+    def is_operate(c):
+        return (c == '+') | (c == '-')
+
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.calculate("2+4-1"))
+    print(solution.calculate("(1)"))
